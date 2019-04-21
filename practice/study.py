c31d4d90f9be2e4e0fa31cab3ff5d1068bcab525
18801089593 <18801089593@163.com>
2019-04-21 21:36:30 +0800

    4.21日练习

---
+++ practice/study.py
@@ -0,0 +1,11 @@
+# # a=5
+# # print(a)
+# a,b=input().split(",")
+# print(a,b)
+# print(type(a))
+# print(type(b))
+# a=int(a)
+# print(type(a))
+a,b=input("请输入姓名和年龄,用逗号隔开").split(",")
+#print("姓名是："+a+","+"年龄是:"+b)
+print("%s 的年龄是 %d 岁"%(a,int(b)))
