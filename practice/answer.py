a87d248aea0c3e6e64c1a3b5869c29c9d4a1455e
18801089593 <18801089593@163.com>
2019-05-18 13:34:06 +0800

    5月15日

---
+++ practice/answer.py
@@ -0,0 +1,55 @@
+#1.打印小猫爱吃鱼，小猫要喝水
+
+# class Cat():
+#
+#
+#     def eat(self):
+#         print("吃鱼")
+#     def drink(self):
+#         print("喝水")
+#
+#
+#
+#
+# class Dog(Cat):
+#     def __init__(self,ww):
+#         self.tt=ww
+#     def eat(self):
+#         print("吃骨头")
+#     def wang(self):
+#         print("%s小狗旺旺"%(self.tt))
+#     def sleep(self):
+#         print("%s小狗沟沟"%(self.tt))
+# b=Dog("我们家")
+# b.wang()
+# b.drink()
+#b.eat()
+
+
+
+
+
+
+
+
+
+#1.打印小猫爱吃鱼，小猫要喝水
+
+# class Cat():
+    # def __init__(self,name):
+    #     self.catname=name
+
+    # def eat(self):
+    #     print("%s爱吃鱼"%(self.catname))
+    # def drink(self):
+    #     print("%s爱喝水"%(self.catname))
+# a=Cat("小猫")
+# a.eat()
+# a.drink()
+
+
+
+
+
+
+
